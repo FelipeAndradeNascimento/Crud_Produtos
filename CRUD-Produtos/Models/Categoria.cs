41b3800f6ea653161548fb70ca610e67fc1d2a1c
felipe de andrade nascimento <wolvif@gmail.com>
2021-01-27 19:09:20 -0300

    Ajustando estrutura e Criando Model Categoria

---
+++ CRUD-Produtos/Models/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUD_Produtos.Models.Categoria
+{
+    [Table("Categorias")]
+    public class Categoria
+    {
+        public int IdCategoria { get; set; }
+        
+        [Display(Name = "Nome da Categoria")]
+        public string Ctg_nome { get; set; }
+        public string Ctg_descricao { get; set; }
+        public DateTime Ctg_dtCadastro { get; set; }
+        
+        public List<Produto> Produtos { get; set; }
+
+    }
+}
