830647e058599e8295129c5baf816fc49b95fbf8
felipe de andrade nascimento <wolvif@gmail.com>
2021-01-27 19:57:19 -0300

    Criando Controller para Produto e View Lista de Produtos

---
+++ CRUD-Produtos/Controllers/ProdutosController.cs
@@ -0,0 +1,98 @@
+﻿using CRUD_Produtos.Models;
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace CRUD_Produtos.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        ProdutoContext db;
+
+        public ProdutosController()
+        {
+            db = new ProdutoContext();
+        }
+
+        // GET: Produtos
+        public ActionResult Index()
+        {
+            var produtos = db.Produtos.ToList();
+            return View(produtos);
+        }
+
+        // GET: Produtos/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Produtos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Produtos/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Produtos/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Produtos/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Produtos/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Produtos/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
